8454e2b1436dfdf1c337ce89fefb032356435486
Awais Iqbal Qadri <42121572+Awaisqadri22@users.noreply.github.com>
2025-03-04 14:02:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,104 @@
+const express = require("express");
+const app = express();
 require("dotenv").config();
-const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", false);
+const Note = require("./models/note");
 
-const url = process.env.MONGODB_URI;
+app.use(express.static("dist"));
 
-console.log("connecting to", url);
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB...");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
   });
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-module.exports = mongoose.model("Note", noteSchema);
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
